2f77bc55446b81c49e2a339b9fcb3e602758a984
cyberAkeshan <akeshankunarajah@gmail.com>
2024-05-04 19:25:00 +0200

    empty project structure

---
+++ taskmanagement/plugins/src/main/java/de/dhbw/softwareengineering/taskmanagement/MainApplication.java
@@ -0,0 +1,16 @@
+package de.dhbw.softwareengineering.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "de.dhbw.softwareengineering.taskmanagement.plugins")
+@EntityScan("de.dhbw.softwareengineering.writingorganizer.plugins")
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
